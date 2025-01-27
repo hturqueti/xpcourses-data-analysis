c0cc0656bdb8556c42efdc962961ad3536cf0d10
Henrique Marques Turqueti <hturqueti@hotmail.com>
2025-01-27 12:41:54 -0300

    feat: Function to parse dates from multiple formats

--- src/utils.py
+++ src/utils.py
@@ -10,3 +10,20 @@ project_path = Path("__file__").resolve().parents[1]
 def load_parameters(file_path: Union[Path, str] = project_path.joinpath("parameters.yaml")) -> dict:
     with open(file_path, 'r') as file:
         return yaml.safe_load(file)
+
+import pandas as pd
+
+def parse_date(date_str: str) -> pd.Timestamp:
+    """
+    Parses a string date in 'dd/mm/yyyy' or 'yyyy-mm-dd' format and returns a datetime object.
+
+    Args:
+        date_str: The date in string.
+
+    Returns:
+        A datetime object representing the date.
+    """
+    try:
+        return pd.to_datetime(date_str, format='%d/%m/%Y').date()
+    except ValueError:
+        return pd.to_datetime(date_str, format='%Y-%m-%d').date()
